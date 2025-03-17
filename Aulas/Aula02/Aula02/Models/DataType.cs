a2b60e0161672555ff9ff86eadddc5a57395307f
cbGabriel9 <gaby.ceronbianchi@gmail.com>
2025-03-17 20:01:16 -0300

    Updte

---
+++ Aulas/Aula02/Aula02/Models/DataType.cs
@@ -0,0 +1,30 @@
+﻿namespace Aula02.Models
+{
+    public class DataType
+    {
+        public char myVar = 'a';
+
+        public const char myConst = 'b';
+
+        public char myChar1 = 'f';
+        public char myChar2 = ':';
+        public char myChar3 = 'x';
+
+        // Podemos também atribuir referência de caracteres Unicode
+
+        public char myChar4 = '\u2726';
+
+        // Podemos ainda mesclar caracteres especiais como, nova linha, tabulação e etc.
+
+        string textline = "Esta é uma linha de texto.\n\n\nE esta é outra linha";
+
+        /*
+          \e - Caractere de escape
+          \n - Nova linha
+          \r - Retorno
+          \t - Tabulação horizontal
+          \" - Aspas duplas, usadas para exibir aspas dentro de uma string
+          \' - Aspas simples, usadas para exibir aspa simples na string
+        */
+    }
+}
